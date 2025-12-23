4326f99776a146ff22464d984b17c50b4a865dbd
Joel Wetzell <git@jwetzell.com>
2025-12-22 20:22:31 -0600

    add style code table

---
+++ style.go
@@ -0,0 +1,11 @@
+package artnet
+
+var (
+	StNode       uint8 = 0x00
+	StController uint8 = 0x01
+	StMedia      uint8 = 0x02
+	StRoute      uint8 = 0x03
+	StBackup     uint8 = 0x04
+	StConfig     uint8 = 0x05
+	StVisual     uint8 = 0x06
+)
